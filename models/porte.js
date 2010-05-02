88ee04bb3c85eb0037c38c15c00db3c6b1f5405a
jdkoeck <jdkoeck@localhost>
2010-05-02 17:06:31 +0200

    porte.js

---
+++ models/porte.js
@@ -0,0 +1,48 @@
+var opened = false;
+var blocked = false;
+
+function initialize() {
+    Browser.essai = 3;
+}
+
+function open(time) {
+  if(!opened) {
+	opened = true;
+	doorRotInterp.keyValue[0][3] = 0;
+	doorRotInterp.keyValue[1][3] = -1.57;
+  }
+  doorRotTime.set_startTime = time; 
+}
+
+function close(time) {
+  if(opened) {
+    opened = false;
+	doorRotInterp.keyValue[0][3] = -1.57;
+	doorRotInterp.keyValue[1][3] = 0;
+  }
+  doorRotTime.set_startTime = time; 
+}
+
+function trigger(time) {
+    if(blocked)
+	return;
+    if(!opened) {
+      open(time);
+    } else {
+      close(time);
+    }
+}
+
+function isActive(b, time) {
+    blocked = b;
+    if(!b) {
+	Browser.callPendingAction(time);
+    }
+}
+
+function obj() {
+    var door = new Object();
+    door.open = open;
+    door.close = close;
+    return door;
+}
